51459e3e40631cb7376a0a20e9622e0ff7c2f316
Zachary Newman <z@znewman.net>
2021-09-28 17:49:40 -0400

    Add CI setup for Rust

--- nix/default.nix
+++ nix/default.nix
@@ -18,11 +18,11 @@ let
     cargo = rust;
   };
 in {
-  inherit pkgs src;
+  inherit pkgs src rust;
 
   # provided by shell.nix
   devTools = {
-    inherit (pkgs) niv nixfmt;
+    inherit (pkgs) niv nixfmt nix-linter;
     inherit (pre-commit-hooks) pre-commit;
     inherit rust;
   };
@@ -35,6 +35,21 @@ in {
         shellcheck.enable = true;
         nixfmt.enable = true;
         nix-linter.enable = true;
+        # Really should override pre-commit-hooks tools to use my Rust version rather than cloning.
+        my-rustfmt = {
+          enable = true;
+          entry =
+            "bash -c 'PATH=${rust}/bin ${rust}/bin/cargo fmt -- --check --color always'";
+          pass_filenames = false;
+          files = "\\.rs$";
+        };
+        my-clippy = {
+          enable = true;
+          entry =
+            "bash -c 'PATH=${rust}/bin ${rust}/bin/cargo clippy --features strict -- --no-deps'";
+          pass_filenames = false;
+          files = "\\.rs$";
+        };
       };
       # generated files
       excludes = [ "^nix/sources.nix$" ];
@@ -47,5 +62,7 @@ in {
 
     pname = "scalingsnapshots";
     version = "0.1";
+
+    doCheck = true; # run `cargo test`
   };
 }
--- nix/rust.nix
+++ nix/rust.nix
@@ -4,6 +4,8 @@ let
     import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
   channel = "nightly";
   date = "2021-09-22";
-  targets = [ ];
-  chan = pkgs.rustChannelOfTargets channel date targets;
+  extensions = [ "rust-analyzer-preview" "clippy-preview" "rust-src" ];
+  chan = (pkgs.rustChannelOf { inherit channel date; }).rust.override {
+    inherit extensions;
+  };
 in chan
