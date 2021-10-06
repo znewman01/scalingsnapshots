dbad1302b96ebf7cc1d1a81db9242a07344a8bc3
Zachary Newman <z@znewman.net>
2021-10-06 13:41:29 -0400

    Add CLI args to sssim

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
+use clap::{
+    crate_authors, crate_description, crate_license, crate_name, crate_version, AppSettings, Clap,
+};
+
+#[derive(Clap)]
+#[clap(name = crate_name!(), author=crate_authors!(", "), version=crate_version!())]
+#[clap(license=crate_license!(), about=crate_description!())]
+#[clap(setting=AppSettings::ColoredHelp)]
+struct Args {}
+
 fn main() {
+    let _args: Args = Args::parse();
     println!("Hello, world!");
 }
 
